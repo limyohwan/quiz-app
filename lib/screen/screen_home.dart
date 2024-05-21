d0f6dab383a94e67bbd4b8c46238b1eaccfb93c5
Yohwan Lim <dyghks7102@naver.com>
2024-05-21 23:58:16 +0900

    feat: 문구 추가

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -27,10 +27,54 @@ class _HomeScreenState extends State<HomeScreen> {
               'images/quiz.png', 
               width: width * 0.8,
               ),
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024)
+            ),
+            Text(
+              '플러터 퀴 앱',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              '퀴즈를 풀기 전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048)
+            ),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개'),
+            _buildStep(width, '2. 랜덤으로 나오는 퀴즈 3개'),
+            _buildStep(width, '3. 랜덤으로 나오는 퀴즈 3개')
           ],
         )
       ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048, 
+        width * 0.024, 
+        width * 0.048, 
+        width * 0.024
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024)
+          ),
+          Text(title),
+        ],
+      )
     );
   }
 }
